18f06a88fcab5e1c165ea15dc808bbaebdb190e1
devHenrique <devherique.ramos@gmail.com>
2024-10-25 22:04:51 -0300

    implement service registry pattern and api gateway pattern

---
+++ service-registry/src/test/java/com/rentalmovie/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentalmovie.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
